255dc6de0556e7bb8c6d9d7761a726cac0e22194
제다빈 <notbad1700@gmail.com>
2026-03-25 14:43:58 +0900

    [Gold IV] Title: 연구소, Time: 3760 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -7,25 +7,30 @@ N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 temp = [list(row) for row in board]
 
-candi = [(i, j) for i in range(N) for j in range(M) if board[i][j] == 0]
+virus = []
+candi = []
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 2:
+            virus.append((i, j))
+        elif board[i][j] == 0:
+            candi.append((i, j))
 
 max_cnt = 0
-for wall in combinations(candi, 3):
-    for x, y in wall:
+for i in combinations(range(len(candi)), 3):
+    for idx in i:
+        x, y = candi[idx]
         board[x][y] = 1
 
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == 2:
-                deq = deque([(i, j)])
+    deq = deque(virus)
 
-                while deq:
-                    x, y = deq.popleft()
-                    for dx, dy in dxy:
-                        nx, ny = x + dx, y + dy
-                        if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
-                            board[nx][ny] = 2
-                            deq.append((nx, ny))
+    while deq:
+        x, y = deq.popleft()
+        for dx, dy in dxy:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
+                board[nx][ny] = 2
+                deq.append((nx, ny))
 
     cnt = 0
     for i in range(N):
