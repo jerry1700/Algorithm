edd07fda779e92c801bfb24aee2dd9996e18fdac
제다빈 <notbad1700@gmail.com>
2026-04-06 15:48:35 +0900

    [Silver V] Title: 배열 합치기, Time: 2080 ms, Memory: 187436 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,5 +1,18 @@
 N, M = map(int, input().split())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+A = sorted(list(map(int, input().split())))
+B = sorted(list(map(int, input().split())))
 
-print(*sorted(A + B))
+i, j = 0, 0
+result = []
+
+while i < len(A) and j < len(B):
+    if A[i] < B[j]:
+        result.append(A[i])
+        i += 1
+    else:
+        result.append(B[j])
+        j += 1
+result.extend(A[i:])
+result.extend(B[j:])
+
+print(*result)
