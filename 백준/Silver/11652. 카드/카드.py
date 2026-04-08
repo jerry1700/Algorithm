0de7d0028d46466aea81dcffaa16c2dd20076706
제다빈 <notbad1700@gmail.com>
2026-04-08 11:46:19 +0900

    [Silver IV] Title: 카드, Time: 2496 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -1,9 +1,20 @@
-from collections import Counter
-
 N = int(input())
-nums = [int(input()) for _ in range(N)]
+nums = sorted([int(input()) for _ in range(N)])
 
-count = Counter(nums)
-result = sorted(count.items(), key = lambda x: (-x[1], x[0]))
+cnt = 0
+mxval = -(2 ** 62) - 1
+mxcnt = 0
 
-print(result[0][0])
+for i in range(N):
+    if i == 0 or nums[i - 1] == nums[i]:
+        cnt += 1
+    else:
+        if cnt > mxcnt:
+            mxval = nums[i - 1]
+            mxcnt = cnt
+        cnt = 1
+
+if cnt > mxcnt:
+    mxval = nums[i - 1]
+
+print(mxval)
