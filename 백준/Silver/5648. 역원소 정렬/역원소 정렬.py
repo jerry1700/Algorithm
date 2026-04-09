7a7766ea08daeff99b25a4c75b2547d06fb80c6d
제다빈 <notbad1700@gmail.com>
2026-04-09 16:11:59 +0900

    [Silver V] Title: 역원소 정렬, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/5648. 역원소 정렬/역원소 정렬.py
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.py
@@ -4,16 +4,11 @@ def input():
     return sys.stdin.readline().rstrip()
 
 tmp = input().split()
-n = int(tmp[0])
-nums = tmp[1:]
-while True:
-    if len(nums) == n:
-        break
+n, nums = int(tmp[0]), tmp[1:]
+while len(nums) < n:
+    nums.extend(input().split())
 
-    tmp = input().split()
-    nums += tmp
-
-for idx, i in enumerate(nums):
-    nums[idx] = int(i[::-1])
+for idx, num in enumerate(nums):
+    nums[idx] = int(num[::-1])
 
 print(*sorted(nums), sep = "\n")
