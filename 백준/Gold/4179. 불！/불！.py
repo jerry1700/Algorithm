7828578d09f9831603f4381b323209596ca997d6
제다빈 <notbad1700@gmail.com>
2026-04-22 09:34:31 +0900

    [Gold III] Title: 불!, Time: 2328 ms, Memory: 73476 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,52 +1,46 @@
-import sys
 from collections import deque
 
 dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-R, C = map(int, input().split())
-board = [list(input()) for _ in range(R)]
+def escape(maze, fire, i, j):
+    jihoon = [[-1] * C for _ in range(R)]
 
-jstart = []
-fstart = []
-for i in range(R):
-    for j in range(C):
-        if board[i][j] == "J":
-            jstart.append((i, j))
-        if board[i][j] == "F":
-            fstart.append((i, j))
+    deq = deque([(i, j)])
+    jihoon[i][j] = 0
 
-fdeq = deque(fstart)
-fires = [[-1] * C for _ in range(R)]
-for x, y in fstart:
-    fires[x][y] = 0
-
-while fdeq:
-    x, y = fdeq.popleft()
-    for dx, dy in dxy:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < R and 0 <= ny < C and fires[nx][ny] == -1 and board[nx][ny] == ".":
-            fires[nx][ny] = fires[x][y] + 1
-            fdeq.append((nx, ny))
-
-jdeq = deque(jstart)
-jihoons = [[-1] * C for _ in range(R)]
-for x, y in jstart:
-    jihoons[x][y] = 0
-
-def bfs(R, C, fires, jihoons, jdeq):
-    while jdeq:
-        x, y = jdeq.popleft()
+    while deq:
+        x, y = deq.popleft()
         for dx, dy in dxy:
             nx, ny = x + dx, y + dy
-            if nx < 0 or nx >= R or ny < 0 or ny >= C:
-                return jihoons[x][y] + 1
-            if jihoons[nx][ny] >= 0 or board[nx][ny] == "#":
-                continue
-            if fires[nx][ny] != -1 and fires[nx][ny] <= jihoons[x][y] + 1:
-                continue
-            jihoons[nx][ny] = jihoons[x][y] + 1
-            jdeq.append((nx, ny))
+            if not (0 <= nx < R and 0 <= ny < C):
+                return jihoon[x][y] + 1
+            if 0 <= nx < R and 0 <= ny < C and jihoon[nx][ny] == -1 and maze[nx][ny] != "#" and (fire[nx][ny] == -1 or jihoon[x][y] + 1 < fire[nx][ny]):
+                jihoon[nx][ny] = jihoon[x][y] + 1
+                deq.append((nx, ny))
+            
     return "IMPOSSIBLE"
 
-answer = bfs(R, C, fires, jihoons, jdeq)
+R, C = map(int, input().split())
+maze = [list(input()) for _ in range(R)]
+
+fire_location = [(i, j) for i in range(R) for j in range(C) if maze[i][j] == "F"]
+fire = [[-1] * C for _ in range(R)]
+
+deq = deque(fire_location)
+for i, j in fire_location:
+    fire[i][j] = 0
+
+while deq:
+    x, y = deq.popleft()
+    for dx, dy in dxy:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < R and 0 <= ny < C and fire[nx][ny] == -1 and maze[nx][ny] != "#":
+            fire[nx][ny] = fire[x][y] + 1
+            deq.append((nx, ny))
+
+for i in range(R):
+    for j in range(C):
+        if maze[i][j] == "J":
+            answer = escape(maze, fire, i, j)
+
 print(answer)
