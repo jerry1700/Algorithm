9d14a1046d147a00f51163445e39d498498e715a
제다빈 <notbad1700@gmail.com>
2026-04-27 14:24:16 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -4,9 +4,7 @@ dp = [0] * (n + 1)
 dp[1] = 1
 if n >= 2:
     dp[2] = 3
-if n >= 3:
-    dp[3] = 5
-for i in range(4, n + 1):
-    dp[i] = (dp[i - 2] * 3 + (dp[i - 1] - dp[i - 2])) % 10007
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 2] * 3 + (dp[i - 1] - dp[i - 2])) % 10007
 
 print(dp[n])
