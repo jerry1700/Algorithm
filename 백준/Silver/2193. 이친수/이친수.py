2f9698d2b436ea7038dc578ddce5eda239ff2fa2
제다빈 <notbad1700@gmail.com>
2026-04-27 15:45:53 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,11 +1,12 @@
 N = int(input())
 
-dp = [[0] * 10 for i in range(N + 1)]
-
-dp[1][0] = 0
-dp[1][1] = 1
-for x in range(2, N + 1):
-    dp[x][0] += dp[x - 1][0] + dp[x - 1][1]
-    dp[x][1] += dp[x - 1][0]
-
-print(sum(dp[N]))
+dp = [0] * (N + 1)
+dp[1] = 1
+if N >= 2:
+    dp[2] = 1
+if N >= 3:
+    dp[3] = 2
+for i in range(4, N + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+    
+print(dp[N])
