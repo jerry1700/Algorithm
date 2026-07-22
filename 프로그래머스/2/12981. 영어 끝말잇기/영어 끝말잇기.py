a4813ad76b241301d55ff4edcdfd618c5d224577
제다빈 <notbad1700@gmail.com>
2026-07-23 00:57:38 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,6 +1,7 @@
 def solution(n, words):
     wrong = False
     end = words[0][-1]
+    forward = [words[0]]
     for i, word in enumerate(words):
         if i == 0:
             continue
@@ -9,24 +10,14 @@ def solution(n, words):
         if end != start:
             wrong = True
             break
-            
         end = word[-1]
-    
-    overlap = False
-    forward = [words[0]]
-    for j, word in enumerate(words):
-        if j == 0:
-            continue
-            
+
         if word in forward:
-            overlap = True
+            wrong = True
             break
-            
         forward.append(word)
             
-    if not wrong and not overlap:
-        answer = [0, 0]
-    elif wrong and not overlap:
+    if wrong:
         if (i + 1) % n == 0:
             x = n
         else:
@@ -36,26 +27,7 @@ def solution(n, words):
         else:
             y = (i + 1) // n + 1
         answer = [x, y]
-    elif not wrong and overlap:
-        if (j + 1) % n == 0:
-            x = n
-        else:
-            x = (j + 1) % n
-        if (j + 1) % n == 0:
-            y = (j + 1) // n
-        else:
-            y = (j + 1) // n + 1
-        answer = [x, y]
-    elif wrong and overlap:
-        num = j if i > j else i
-        if (num + 1) % n == 0:
-            x = n
-        else:
-            x = (num + 1) % n
-        if (num + 1) % n == 0:
-            y = (num + 1) // n
-        else:
-            y = (num + 1) // n + 1
-        answer = [x, y]
+    else:
+        answer = [0, 0]
 
     return answer
