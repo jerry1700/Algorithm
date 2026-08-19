b0e633009fcda33522369717f8d57904bf9b618b
제다빈 <notbad1700@gmail.com>
2026-08-19 15:23:15 +0900

    [level 2] Title: 가장 큰 수, Time: 32.19 ms, Memory: 27.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    numbers = sorted([str(i) for i in numbers], key=lambda x: x * 3, reverse=True)
+    answer = ''.join(numbers)
+    
+    if answer[0] == '0':
+        return '0'
+        
+    return answer
